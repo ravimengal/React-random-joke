312de85e247e7f69e7485ec781ad0b82d613fb0f
ravimengal <mengal22ravi@gmail.com>
2021-05-14 12:13:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+
+
+  const [joke, setJoke] = useState(`loading joke`);
+  const [fname, setFN] = useState(`john`);
+  const [sname, setSN] = useState(`Doe`);
+
+  const newJoke = (first, second) => {
+
+    fetch(`http://api.icndb.com/jokes/random?firstName=${first}&lastName=${second}`).
+      then(res => res.json()).
+      then(res2 => {
+        console.warn(res2);
+        setJoke(res2.value.joke)
+      })
+
+  }
+  useEffect(() => {
+
+    newJoke(fname, sname)
+
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <div className='App'>
+      <h1>Random Joke</h1>
+      
+      <input type="text"  placeholder='enter name'   value={fname} onChange={(e) => setFN(e.target.value)} />
+      <input type="text"  placeholder='enter lastname' value={sname} onChange={(e) => setSN(e.target.value)} />
+      <button onClick={() => newJoke(fname, sname)}> Click for new joke</button>
+
+      <h1>{joke}</h1>
+      
+
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
+
